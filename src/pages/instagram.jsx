b4f8074dd7406905bac6496b2801ce8f40f99509
livanshu-saini <livanshu0827.be21@chitkara.edu.in>
2024-04-14 16:41:11 +0530

    video bug fixed

--- src/pages/instagram.jsx
+++ src/pages/instagram.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import instaReel from "../assets/insta.mp4"
+import instaReel from "../assets/insta.mp4";
 import { FaInstagram } from "react-icons/fa";
 
 const InstagramPage = () => {
   return (
-    <div className='flex flex-row flex-1 justify-center items-center '>
-      <div className='flex-1 justify-center items-center flex'>
-        <video src={instaReel}
+    <div className="flex flex-row flex-1 justify-center items-center ">
+      <div className="flex-1 justify-center items-center flex">
+        <video
+          src={instaReel}
           autoPlay={true}
-          // controls
+          muted={true} // Ensure the video is muted
           loop={true}
-          className='h-[75vh] rounded-xl bx_shadow'
-          
-        ></video>
+          className="h-[75vh] rounded-xl bx_shadow"
+        />
       </div>
-      <div className='flex flex-1 flex-col justify-center items-center gap-8'>
-        <div className='flex flex-col gap-2 justify-center items-center'>
-          <h1 className='text-4xl tx_shadow'>
+      <div className="flex flex-1 flex-col justify-center items-center gap-8">
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <h1 className="text-4xl tx_shadow">
             a lot more to see, a lot more to shoot
           </h1>
-          <h4 className='text-xl'>showcasing some frames in my vicinity</h4>
+          <h4 className="text-xl">showcasing some frames in my vicinity</h4>
         </div>
-        <button className='flex gap-2 justify-center items-center bg-bgWhite p-2 rounded-sm'>
-          <FaInstagram 
-            size={32}
-          />
-          <span className='text-2xl'>instagram</span>
+        <button className="flex gap-2 justify-center items-center bg-bgWhite p-2 rounded-sm">
+          <FaInstagram size={32} />
+          <span className="text-2xl">instagram</span>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InstagramPage
+export default InstagramPage;
