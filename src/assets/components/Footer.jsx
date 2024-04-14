2aaeecb9b62d8640e27b7eb8859513bb7933a1ae
livanshu-saini <livanshu0827.be21@chitkara.edu.in>
2024-04-14 16:38:34 +0530

    don't know change

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ()=>{
+    return(
+        <div className='main-footer w-screen'>
+            <div className=' bg-black color text-white flex justify-around p-3'>
+                <p>made with ❤️ by livanshu aka photicted</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
